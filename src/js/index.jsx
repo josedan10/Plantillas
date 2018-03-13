6fd180242c473ab39252bca26890b2cdc2ec2572
josedanCanaima <josedanq100@gmail.com>
2018-03-13 08:09:40 -0400

    TImer apart

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,19 +3,15 @@ import ReactDOM from 'react-dom';
 // import Nav from './nav/nav';
 import Nav from './nav/nav-menu';
 
-$(document).ready(function () {
+// Links para el menu y las redes
+const links = ['Home', 'Services', 'Selected Works', 'Own Community', 'Team', 'Clients', 'Contact'];
+// const redes = ['twitter', 'facebook', 'instagram', 'youtube'];
 
+// Imagen del logo
+const logo = 'src/img/logo.png'; 					
 
-	// Links para el menu y las redes
-	const links = ['Home', 'Services', 'Selected Works', 'Own Community', 'Team', 'Clients', 'Contact'];
-	// const redes = ['twitter', 'facebook', 'instagram', 'youtube'];
-
-	// Imagen del logo
-	const logo = 'src/img/logo.png'; 					
-
-	ReactDOM.render(
-		<Nav tipo='nav-left' logo={logo} links={links} />,
-		document.getElementById('nav')
-	);
-});
+ReactDOM.render(
+	<Nav tipo='nav-left' logo={logo} links={links} />,
+	document.getElementById('nav')
+);
 
--- src/js/timer.config.jsx
+++ src/js/timer.config.jsx
@@ -2,20 +2,18 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Timer from './timer/timer';
 
-$(document).ready(function () {
 
-    	// Datos para el timer
-	const timer = {
-		fecha: '2018-06-22', horaFinal: '00:00'					
-	};
+// Datos para el timer
+const timer = {
+	fecha: '2018-03-22', horaFinal: '02:29'					
+};
 
-	function tick() {
-		ReactDOM.render(
-			<Timer fecha={timer.fecha} horaFinal={timer.horaFinal} />,
-			document.getElementById('timer')
-		);
-	}
+function tick() {
+	ReactDOM.render(
+		<Timer fecha={timer.fecha} horaFinal={timer.horaFinal} />,
+		document.getElementById('timer')
+	);
+}
 
-	setInterval(tick, 1000);
+setInterval(tick, 1000);
 
-});
