066c906954861455aa88664afc823cf9c0f13e30
josedanCanaima <josedanq100@gmail.com>
2018-03-13 20:07:12 -0400

    Recompilado

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -10,23 +10,8 @@ const links = ['Home', 'Services', 'Selected Works', 'Own Community', 'Team', 'C
 // Imagen del logo
 const logo = 'src/img/logo.png'; 					
 
-<<<<<<< HEAD
 ReactDOM.render(
 	<Nav tipo='nav-left' logo={logo} links={links} />,
 	document.getElementById('nav')
 );
-=======
-	// Links para el menu y las redes
-	const links = ['Home', 'Services', 'Selected Works', 'Own Community', 'Team', 'Clients', 'Contact'];
-	// const redes = ['twitter', 'facebook', 'instagram', 'youtube'];
-
-	// Imagen del logo
-	const logo = 'src/img/logo.svg'; 					
-
-	ReactDOM.render(
-		<Nav tipo='nav-left' logo={logo} links={links} />,
-		document.getElementById('nav')
-	);
-});
->>>>>>> b833780169326ed577f9ef548a892fcd57c1acde
 
